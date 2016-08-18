55934a123ab2d0ef2f1bd54fb246f52eb2bc3fd5
Shakka <tosmith1997@gmail.com>
2016-08-18 14:03:31 +0200

    Changed scripts to work on AWS

--- scripts/compilelearning.sh
+++ scripts/compilelearning.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#yum -y install httpd > /var/log/installapache.out 2>&1
 . /etc/environment
 cd /var/grassroot
 gradle build
--- scripts/startlearning.sh
+++ scripts/startlearning.sh
@@ -1,2 +1,8 @@
-cd Projects/Grassroot/grassroot-learning
-nohup java -jar build/libs/gs-rest-service-0.1.0.jar
+#!/bin/bash
+. /etc/environment
+
+CURR=$PWD
+cd /var/grassroot
+nohup java -jar build/libs/gs-rest-service-0.1.0.jar > grassroot-learning.log 2>&1 &
+echo $! > .pid
+cd $CURR

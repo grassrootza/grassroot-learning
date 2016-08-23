ab68edddb75f9137c31c5d95bb80fb3c090feee7
Shakka <tosmith1997@gmail.com>
2016-08-23 12:46:16 +0200

    added script that runs monthly to see what selo missed

---
+++ scripts/selo_test.py
@@ -0,0 +1,71 @@
+import requests, time, psycopg2, csv, re, os
+from slacker import Slacker
+
+csv_filename = "ussd_date_entry_" + time.strftime('%b%d') + '.csv'
+global last_month_filename
+last_month_filename= 'hist/selo_errors.txt'
+global this_month_filename
+
+slack = Slacker(os.environ['SLACK_AUTH_TOKEN'])
+
+
+def get_log_from_db():
+    conn_string = os.environ['CONN_STR']
+    conn = psycopg2.connect(conn_string)
+    cursor = conn.cursor()
+    cursor.execute(os.environ['QUERY_STR'])
+    records = cursor.fetchall()
+
+    with open(csv_filename, 'w') as f:
+        writer = csv.writer(f, delimiter=',')
+        for row in records:
+            writer.writerow(row)
+
+
+def parse_file(filename):
+    with open(filename) as infile, open('tmp/parsed_logs.txt', 'w') as outfile:
+        for line in infile:
+            d = re.search('input: [[a-zA-Z0-9.-@ ]*]*', line)
+            if d:
+                d = d.group(0)
+                date = d[7:]
+                outfile.write(date)
+                outfile.write('\n')
+    return 'tmp/parsed_logs.txt'
+
+
+def get_results_file(log_file):
+    with open(log_file) as infile, open(this_month_filename, 'w') as outfile:
+        for line in infile:
+            # u = 'http://52.210.114.216:9000/parse?phrase=' + line
+            url = 'http://localhost:9000/parse?phrase=' + line
+            data = requests.get(url)
+
+            if data.text == 'ERROR_PARSING':
+                outfile.write(line)
+
+
+def get_differences_from_last_month(this_month, last_month):
+    with open(this_month, 'r') as file1:
+        with open(last_month, 'r') as file2:
+            diff = set(file2).difference(file1)
+    diff.discard('\n')
+    with open(this_month, 'a') as file1:
+        file1.write('\n' + '***** NEW THIS MONTH *****' + '\n')
+        file1.writelines(diff)
+
+
+def main():
+    get_log_from_db()
+    get_results_file(parse_file(csv_filename))
+    get_results_file(parse_file('ussd_date_entry_aug14.csv'))
+    get_differences_from_last_month(this_month_filename, last_month_filename)
+    slack.files.upload(this_month_filename, channels='#learning')
+
+
+while True:
+    this_month_filename = 'hist/selo_errors_' + time.strftime('%b%d') + '.txt'
+    main()
+    last_month_filename = this_month_filename
+    print ':D'
+    time.sleep(2592000)

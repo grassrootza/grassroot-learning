73cbcad01f13cfbca6a2a1398c1c7c36dba2b790
Luke Jordan <luke.jordan@grassroot.org.za>
2016-10-09 19:20:20 +0200

    Added Apache Commons Math to project

--- src/main/java/za/org/grassroot/learning/worddist/DistanceMeasurer.java
+++ src/main/java/za/org/grassroot/learning/worddist/DistanceMeasurer.java
@@ -1,5 +1,7 @@
 package za.org.grassroot.learning.worddist;
 
+import org.apache.commons.math3.linear.ArrayRealVector;
+import org.apache.commons.math3.linear.RealVector;
 import org.ejml.simple.SimpleMatrix;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,6 +18,7 @@ public class DistanceMeasurer {
     public void readInVector() {
         try {
             SimpleMatrix simpleMatrix = SimpleMatrix.loadCSV("csvFile.txt"); // todo : read this location from properties file
+            RealVector apacheCommonsRealVector = new ArrayRealVector();
         } catch (IOException e) {
             logger.info("Error loading matrix from file!");
             e.printStackTrace();

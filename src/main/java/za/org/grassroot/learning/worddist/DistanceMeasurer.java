c1d45322112aa2ec999415a19b973c0ab5771b65
Orien Zeng <ozeng@stanford.edu>
2016-10-16 02:23:30 -0700

    Add W matrix generation to DistanceMeasurer

--- src/main/java/za/org/grassroot/learning/worddist/DistanceMeasurer.java
+++ src/main/java/za/org/grassroot/learning/worddist/DistanceMeasurer.java
@@ -25,4 +25,62 @@ public class DistanceMeasurer {
         }
     }
 
+	private static RealMatrix generate(HashMap<String, Integer> vocab, HashMap<Integer, String> ivocab) {
+		RealMatrix W = null;
+		try {
+	        Scanner vocabInput = new Scanner(new File("vocab.txt"));
+	        ArrayList<String> words = new ArrayList<String>();
+	        while (vocabInput.hasNextLine()) {
+	            String line = vocabInput.nextLine();
+	            line = line.split(" ")[0];
+	            words.add(line);
+	        }
+	        vocabInput.close();
+	        Scanner vectorInput = new Scanner(new File("vectors.txt"));
+	        HashMap<String, ArrayList<Double>> vectors = new HashMap<String, ArrayList<Double>>();
+	        while (vectorInput.hasNextLine()) {
+	            String line = vectorInput.nextLine();
+	            String vecArray[] = line.split(" ");
+	            String word = vecArray[0];
+	            ArrayList<Double> wordVec = new ArrayList<Double>();
+	            for (int i = 1; i < vecArray.length; i++) {
+                    wordVec.add(Double.parseDouble(vecArray[i]));
+	            }
+	            vectors.put(word, wordVec);
+	        }
+	        vectorInput.close();
+	        int vocab_size = words.size();
+	        for (int i = 0; i < words.size(); i++) {
+	        	vocab.put(words.get(i), i);
+	        	ivocab.put(i, words.get(i));
+	        }
+	        int vector_dim = vectors.get(ivocab.get(0)).size();
+	        W = MatrixUtils.createRealMatrix(vocab_size, vector_dim);
+	        for(String word : vectors.keySet()) {
+	        	if (word.equals("<unk>")) {
+	        		continue;
+	        	}
+	        	ArrayList<Double> wordVector = vectors.get(word);
+	        	for (int i = 0; i < wordVector.size(); i++) {
+	        		W.setEntry(vocab.get(word), i, wordVector.get(i));
+	        	}
+	        }
+	        
+	        RealVector d = new ArrayRealVector();
+	        for (int i = 0; i < W.getRowDimension(); i++) {
+	        	double sum = 0;
+	        	for (int j = 0; j < W.getColumnDimension(); j++) {
+	        		sum += Math.pow(W.getEntry(i, j), 2);
+	        	}
+	        	d = d.append(Math.sqrt(sum));
+	        }
+	        for (int i = 0; i < W.getColumnDimension(); i++) {
+	        	W.setColumnVector(i, W.getColumnVector(i).ebeDivide(d));
+	        }
+	    } catch (FileNotFoundException ex) {
+	        ex.printStackTrace();
+	    }
+		return W;
+	}
+
 }

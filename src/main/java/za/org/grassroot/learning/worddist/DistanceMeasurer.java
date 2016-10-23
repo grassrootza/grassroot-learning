f5fd4fa59838c167d0d951fa5b489d6e633b4c2c
Orien Zeng <ozeng@stanford.edu>
2016-10-23 03:37:52 -0700

    Filter output either by distance or by quantity

--- src/main/java/za/org/grassroot/learning/worddist/DistanceMeasurer.java
+++ src/main/java/za/org/grassroot/learning/worddist/DistanceMeasurer.java
@@ -1,10 +1,14 @@
 package za.org.grassroot.learning.worddist;
 
+import java.util.Map;
+
 /**
  * Created by luke on 2016/10/16.
  */
 public interface DistanceMeasurer {
 
     void calculateDistances(String searchTerm);
+
+	Map<String, Double> calculateDistances(String searchTerm, int N);
 
 }
--- src/main/java/za/org/grassroot/learning/worddist/DistanceMeasurerImpl.java
+++ src/main/java/za/org/grassroot/learning/worddist/DistanceMeasurerImpl.java
@@ -26,6 +26,7 @@ public class DistanceMeasurerImpl implements DistanceMeasurer {
 
 	private Map<String, Integer> vocab;
 	private Map<Integer, String> iVocab;
+	private Set<String> universe;
 
 	private RealMatrix W;
 
@@ -37,15 +38,62 @@ public class DistanceMeasurerImpl implements DistanceMeasurer {
 		logger.info("Building vectors for word distance service ...");
 		vocab = new HashMap<>();
 		iVocab = new HashMap<>();
+		universe = new HashSet<>();
 		long startTime=System.currentTimeMillis();
 		W = generate(environment.getProperty("grassroot.worddist.vocab.path"),
-				environment.getProperty("grassroot.worddist.vectors.path"));
+				environment.getProperty("grassroot.worddist.vectors.path"),
+				environment.getProperty("grassroot.worddist.universe.path"));
 		long endTime = System.currentTimeMillis();
 		logger.info("Completed word dist initialization in: {} seconds", (endTime - startTime) / 1000);
 	}
 
 	@Override
-	public void calculateDistances(String searchTerm) {
+	public Map<String, Double> calculateDistances(String searchTerm, int N) {
+		
+		List<Double> negativeDist = new ArrayList<>();
+
+		int[] sortedIndices = getSortedDistances(searchTerm, negativeDist);
+
+		// assuming that a.length is what was meant by N
+		Map<String, Double> matches = new LinkedHashMap<String, Double>();
+		int numPrinted = 0;
+		for(int i = 0; i < sortedIndices.length && numPrinted < N; i++) {
+			int wordID = sortedIndices[i]; 
+			String word = iVocab.get(wordID);
+			Double wordDistance = negativeDist.get(wordID);
+			if (!universe.contains(word)) {
+				continue;
+			}
+			logger.info(word + " " + wordDistance);
+			matches.put(word, wordDistance);
+			numPrinted++;
+		}
+		return matches;
+	}
+	
+	public Map<String, Double> calculateDistances(String searchTerm, double minDist) {
+		List<Double> negativeDist = new ArrayList<>();
+
+		int[] sortedIndices = getSortedDistances(searchTerm, negativeDist);
+
+		Map<String, Double> matches = new LinkedHashMap<String, Double>();
+		for(int i = 0; i < sortedIndices.length; i++) {
+			int wordID = sortedIndices[i]; 
+			String word = iVocab.get(wordID);
+			Double wordDistance = negativeDist.get(wordID);
+			if (wordDistance < minDist) {
+				break;
+			}
+			if (!universe.contains(word)) {
+				continue;
+			}
+			logger.info(word + " " + wordDistance);
+			matches.put(word, wordDistance);
+		}
+		return matches;
+	}
+	
+	private int[] getSortedDistances(String searchTerm, List<Double> negativeDist) {
 		RealVector vec_result;
 		if(vocab.containsKey(searchTerm)) {
 			logger.info("Word: " + searchTerm + ", Position in vocabulary: " + vocab.get(searchTerm));
@@ -55,33 +103,27 @@ public class DistanceMeasurerImpl implements DistanceMeasurer {
 			throw new IllegalArgumentException("Word outside of dictionary!");
 		}
 
-		// todo : purpose of vec_norm ?
 		double d = vec_result.getNorm();
 		vec_result = vec_result.mapDivideToSelf(d);
 
 		List<Double> dist = new ArrayList<>();
-		List<Double> negativeDist = new ArrayList<>();
-
+		
 		for (int i = 0; i < W.getRowDimension(); i++) {
 			dist.add(i, W.getRowVector(i).dotProduct(vec_result) * -1);
 			negativeDist.add(-1 * dist.get(i));
 		}
 
-		int[] a = IntStream.range(0, dist.size())
+		int[] sortedIndices = IntStream.range(0, dist.size())
                 .boxed()
 				.sorted((i, j) -> dist.get(i).compareTo(dist.get(j)) )
                 .mapToInt(ele -> ele)
 				.toArray(); // http://stackoverflow.com/questions/4859261/get-the-indices-of-an-array-after-sorting
-
-		// assuming that a.length is what was meant by N
-		for(int i = 0; i < a.length; i++) {
-			int x = a[i];
-			logger.info(iVocab.get(x) + " " + negativeDist.get(x));
-		}
+		return sortedIndices;
 	}
 
-	private RealMatrix generate(String vocabFilePath, String vectorFilePath) {
+	private RealMatrix generate(String vocabFilePath, String vectorFilePath, String universeFilePath) {
 		RealMatrix W = null;
+		
 		try {
 	        Scanner vocabInput = new Scanner(new File(vocabFilePath));
 	        ArrayList<String> words = new ArrayList<String>();
@@ -104,6 +146,13 @@ public class DistanceMeasurerImpl implements DistanceMeasurer {
 	            vectors.put(word, wordVec);
 	        }
 	        vectorInput.close();
+	        Scanner universeInput = new Scanner(new File(universeFilePath));
+	        while (universeInput.hasNextLine()) {
+	            String line = universeInput.nextLine();
+	            line = line.split(" ")[0];
+	            universe.add(line);
+	        }
+	        universeInput.close();
 	        int vocab_size = words.size();
 	        for (int i = 0; i < words.size(); i++) {
 	        	vocab.put(words.get(i), i);
@@ -134,9 +183,15 @@ public class DistanceMeasurerImpl implements DistanceMeasurer {
 	        	W.setColumnVector(i, W.getColumnVector(i).ebeDivide(d));
 	        }
 	    } catch (FileNotFoundException ex) {
+	    	logger.info("Could not find file");
 	        ex.printStackTrace();
 	    }
 		return W;
+	}
+
+	@Override
+	public void calculateDistances(String searchTerm) {
+		calculateDistances(searchTerm, 5);
 	}
 
 }

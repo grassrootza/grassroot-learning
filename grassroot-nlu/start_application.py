3610a93b4a6253cab88ba22fec2e77467d894bf5
FRTNX <frtnxwolfone@outlook.com>
2017-08-03 13:11:53 +0200

    Update start_application.py
    added the db client

--- grassroot-nlu/start_application.py
+++ grassroot-nlu/start_application.py
@@ -1,7 +1,13 @@
 from flask import Flask
+from pymongo import MongoClient  # pymongo allows us to interact with our db
 
 app = Flask(__name__)
+client  = MongoClient() # assumes you have a mongod instance
+                        # running on default host and port
 
+db = client.test_database  # initialises our db, lazily.
+collection = db.test_collection # initialises a table-like
+                                # structure within our db
 
 @app.route('/')
 def hello_world():

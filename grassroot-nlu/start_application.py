4fdd3439751f25d007d6274f1af7ec33e95f2a26
FRTNX <frtnxwolfone@outlook.com>
2017-08-07 10:10:26 +0200

    Update start_application.py

--- grassroot-nlu/start_application.py
+++ grassroot-nlu/start_application.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from pymongo import MongoClient  # pymongo allows us to interact with our db
+import uuid
+import datetime
 
 app = Flask(__name__)
 client  = MongoClient() # assumes you have a mongod instance
@@ -8,7 +10,17 @@ client  = MongoClient() # assumes you have a mongod instance
 db = client.test_database  # initialises our db, lazily.
 collection = db.test_collection # initialises a table-like
                                 # structure within our db
+entries = db.entries
 
+def giveme(text):
+    instance = {'text':text,
+                'date':datetime.datetime.utcnow(),
+                '_id':uuid.uuid4(),
+                'model_found':None
+               }
+    archives.insert_one(instance) # sticks the recieved text+other info into our db
+    # now send the text to our models and rasa    
+      
 @app.route('/')
 def hello_world():
     return 'Here we will put some things!'

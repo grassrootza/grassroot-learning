6f772bc7fb19809b6ad1e03083bd86d2ccd4a727
FRTNX <frtnxwolfone@outlook.com>
2017-08-31 15:25:31 +0200

    Update start_application.py

--- grassroot-nlu/start_application.py
+++ grassroot-nlu/start_application.py
@@ -1,4 +1,4 @@
-from flask import Flask,request, url_for
+from flask import Flask,request, url_for, render_template
 import pymongo
 from pymongo import MongoClient
 from rasa_nlu.config import RasaNLUConfig
@@ -12,7 +12,7 @@ collection = db.collection
 entries = db.entries
 
 #Rasa Settup
-metadata = Metadata.load('/home/frtnx/models/model_20170816-092910')
+metadata = Metadata.load('/home/frtnx/models/model_20170831-114540') #store these in a config file. no need to expose ourfiile system
 interpreter = Interpreter.load(metadata, RasaNLUConfig('/home/frtnx/anaconda3/lib/python3.6/site-packages/rasa_nlu/config_mitie.json'))
 
 #process management
@@ -20,17 +20,65 @@ timer = []
 
 app = Flask(__name__)
 
-@app.route('/parse')
-def parse():
-    text_data = request.args.get('text','')
-    request_data = {'text': text_data}
-    x = identifier(**request_data) #which returns parsed data + uid + date
-    return x 
+def process_identifier(text):
+    x = interpreter.parse(text)
+    value1 = x['intent']['name']
+    value2 = x['entities']
+    if value1 == 'affirm':
+        return 'affirm'
+    else:
+        if value1 == 'None':
+            return 'update'
+        else:
+            return True
 
+@app.route('/')
+def my_form():
+    return render_template("textbox.html")
+
+@app.route('/', methods=["POST"])
+def parse():
+    text_data = request.form['text']
+    uid = request.form['uid']
+    #return str(text_data)
+    prcss = process_identifier(text_data)
+    x = None
+    if prcss == True:   
+        request_data = {'text': text_data}
+        x = identifier(**request_data) #which returns parsed data + uid + date
+        datap = x['parsed']['text']
+        data = [datap]
+        return NQoutput("response.html",var1=str(x), var2=data)
+    elif prcss == 'affirm':
+        x = "Your request is being processed..."
+        return NQoutput("processing.html",var1=x)
+    else:
+        x = transformer(text_data, uid)
+        return str(x)
+
+def transformer(text, uid):
+    try:
+        Uid = []
+        for i in uid:
+            if i != '[' and i != ']' and i != "'":
+                Uid.append(i)
+        uidd = ''.join(Uid)
+        x = entries.find_one({'text': uidd})
+        old_text = x['text']
+        new_text = old_text+ " " + text
+        request_data = {'text': new_text}
+        new_parsed = identifier(**request_data)
+        return NQoutput("response.html", var1=new_parsed, var2=new_text)
+    except:
+        return str(uidd)
+
+
+def NQoutput(template,var1=None, var2=None):
+    return render_template(template, var1=var1, var2=var2) 
 
 def identifier(**request_data):                  # request debuts here
-    start = time.time()
-    timer.append(start)
+    #start = time.time()
+    #timer.append(start)
     recall = check_database(request_data['text'])
     if recall != False:
         return recall                            # suitable entry exists. Return said entry. Process complete.
@@ -47,16 +95,16 @@ def identifier(**request_data):                  # request debuts here
 
 def parser(text, uid, date_time):
     parsed = interpreter.parse(text)
-    end = time.time()
-    timer.append(end)
-    process_time = timer[1] - timer[0]
-    timer.clear()
-    parsed_data = {'parsed': parsed, 'uid': uid, 'date': date_time,'process_time': process_time}
+    #end = time.time()
+    #timer.append(end)
+    #process_time = timer[1] - timer[0]
+    #timer.clear()
+    parsed_data = {'parsed': parsed, 'uid': uid, 'date': date_time} # insert process time here
     with open("event_listener.txt", "a") as myfile:        
         myfile.write(str(parsed_data)+"\n\n")              
     res = update_database(parsed_data)               
-    parsed_data.pop('process_time')
-    return str(parsed_data)                                 # rabbit hole we go. I find the return process beautiful.
+    #parsed_data.pop('process_time')
+    return parsed_data                                 # rabbit hole we go. I find the return process beautiful.
     
 def update_database(new_data):
     entries.update_one({'_id': new_data['uid']}, {"$set": new_data}, upsert=False)
@@ -76,6 +124,7 @@ def check_database(text):
   
 with app.test_request_context():
     print(url_for('parse', text='make your queries here'))
+
 
 if __name__ == '__main__':
     app.run()

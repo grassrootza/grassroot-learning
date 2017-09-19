6410e3a9f8079520cbe40196f29485eb37bcf6b7
FRTNX <frtnxwolfone@outlook.com>
2017-09-19 19:00:56 +0200

    Update start_application.py

--- grassroot-nlu/start_application.py
+++ grassroot-nlu/start_application.py
@@ -1,17 +1,26 @@
 from flask import Flask,request, url_for, render_template
-import pymongo
-from config import interpreter, entries, threshold
+from config import interpreter, threshold,new_model_checker, runtime_training_data
 import uuid, time, datetime, pprint
 from duckling import Duckling
+from poly_database import *
+from poly_Mongo import *
 huidini = Duckling()
 huidini.load()
+import os
+import sys
+import psutil
+import logging
+
 
 app = Flask(__name__)
     
-
+database = MongoDB
 @app.route('/')
 def index():
+    if new_model_checker != []:
+        restart_program()
     return render_template("textbox.html")
+
 
 @app.route('/', methods=["POST"])
 def parse():
@@ -25,6 +34,7 @@ def parse():
         return pep_talk("response.html",var1=str(user_bound), var2=data)
     elif ret_val == 'affirm':
         user_bound = "Your request is being processed..."
+        save_as_training_instance(uid)
         return pep_talk("processing.html",var1=user_bound)
     elif ret_val == 'kamikaze':
         return render_template("textbox.html")
@@ -51,9 +61,10 @@ def process_identifier(text):
     else:
         return True
 
+
 def osiris(new_value, uid):
     try:
-        x = entries.find_one({'uid':uid})
+        x = find_one(database, entries,{'uid':uid})
         old_text = x['past_lives'][0]
         new_text = old_text + " " + new_value
         request_data = {'text': new_text}
@@ -69,9 +80,10 @@ def osiris(new_value, uid):
     except:
         return "Patience and perseverence."
 
+
 def transformer(text, uid):
     try:
-        x = entries.find_one({'uid': uid})
+        x = find_one(database, entries,{'uid': uid})
         old_text = x['text']
         new_text = old_text+ " " + text
         request_data = {'text': new_text}
@@ -87,8 +99,10 @@ def transformer(text, uid):
     except Exception as e:
         return str(e)
 
+
 def pep_talk(template,var1=None, var2=None):
     return render_template(template, var1=var1, var2=var2) 
+
 
 def goldenGates(**request_data):                  
     recall = check_database(request_data['text'])
@@ -101,10 +115,11 @@ def goldenGates(**request_data):
                      'date': str(datetime.datetime.now()),
                      'past_lives': []
 	                }
-        entries.insert_one(new_entry)
+        insert_one(database, entries, new_entry)
         uid = new_entry['_id']
         x = parser(new_entry['text'],uid,new_entry['date'],new_entry['past_lives']) 
         return x
+
 
 def parser(text, uid, date_time,past_life):
     parse = interpreter.parse(text)
@@ -114,14 +129,16 @@ def parser(text, uid, date_time,past_life):
         myfile.write(str(parsed_data)+"\n\n")              
     res = update_database(parsed_data)               
     return parsed_data
+
     
 def update_database(new_data):
     entries.update_one({'_id': new_data['uid']}, {"$set": new_data}, upsert=False)
-    e = entries.find_one({'_id': new_data['uid']})
+    e = find_one(database, entries,{'_id': new_data['uid']})
     return str(e)
+
   
 def check_database(text):
-    previous_entry = entries.find_one({'text': text})
+    previous_entry = find_one(database, entries,{'text': text})
     if previous_entry == None:
         return False
     else:
@@ -130,6 +147,26 @@ def check_database(text):
             return previous_entry
         else:
             return False
+
+
+def save_as_training_instance(uid):
+    dirty = find_one(database, entries,{'uid': uid})
+    cleansed = dirty['parsed']
+    try:
+        cleansed['intent'] = cleansed['intent']['name']
+    except:
+        pass
+    if cleansed['entities'] != []:
+        leng = len(cleansed['entities'])
+        for i in range(0,leng):
+            try:
+                item = cleansed['entities'][i]
+                item.pop('extractor')
+                item.pop('processors')
+            except:
+                pass
+    insert_one(database, runtime_training_data, cleansed)
+
   
 with app.test_request_context():
     print(url_for('parse', text='make your queries here'))
@@ -150,8 +187,20 @@ def formalizer_helper(time_string):
         if parsed[i]['dim'] == 'time':
             new_value = parsed[i]['value']['value']
             return new_value
+  
+
+def restart_program():
+    try:
+        p = psutil.Process(os.getpid())
+        for i in p.open_files + p.connections:
+            os.close(i.fd)
+    except Exception as e:
+        logging.error(e)
+    python = sys.executable
+    print("restarting...")
+    new_model_checker.clear()
+    os.execl(python,python, *sys.argv)
 
 
 if __name__ == '__main__':
     app.run()
-   

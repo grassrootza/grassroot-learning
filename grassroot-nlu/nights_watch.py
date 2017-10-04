70a59688f6618b0fd1b67ecc3f2ff5807576d62c
FRTNX <frtnxwolfone@outlook.com>
2017-10-04 18:17:50 +0200

    Update nights_watch.py

--- grassroot-nlu/nights_watch.py
+++ grassroot-nlu/nights_watch.py
@@ -4,7 +4,7 @@ import time
 
 while True:
     timer = str(datetime.datetime.now())
-    timer = timer[11:16]
+    timer = timer[11:16]   # reader beware. This is a string slice, not a time value
     if timer == this_moment:
         print("Here we go...")
         start_training()

6a8faf4514d5fd706ac2e182334c4eb4cee36903
FRTNX <frtnxwolfone@outlook.com>
2017-10-24 10:35:12 +0200

    Add files via upload

---
+++ grassroot-nlu/databases/db_populator/stub.py
@@ -0,0 +1,4 @@
+black_hole = {'rasa_nlu_data': {'common_examples': [],
+                                 'entity_synonyms': [{'synonyms': ['jozihub', 'JoziHub'], 'value': 'Jozihub'},
+                                                     {'synonyms': ['KYP'], 'value': 'Kliptown Youth Program'}],
+                                 'regex_features': [{'name': 'zipcode', 'pattern': '[0-9]{5}'}]}}

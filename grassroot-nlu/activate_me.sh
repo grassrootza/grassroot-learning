380ef57944d1e4b4a776bccf2544e11a9859d691
FRTNX <frtnxwolfone@outlook.com>
2017-11-07 17:44:20 +0200

    Update activate_me.sh

--- grassroot-nlu/activate_me.sh
+++ grassroot-nlu/activate_me.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 export FLASK_APP=start_application.py
-flask run > output.txt 2>&1 &
+flask run --host=0.0.0.0 > output.txt 2>&1 &
 python trainer.py > training_output.txt 2>&1 &
 python checker.py > system_status.txt 2>&1 &

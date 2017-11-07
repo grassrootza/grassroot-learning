fc437addcebe191226545e2066ea0c89e970ec34
FRTNX <frtnxwolfone@outlook.com>
2017-11-07 09:37:11 +0200

    Delete kill_me.sh

--- kill_me.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash  
-
-ps -ef | grep flask | grep -v grep | awk '{print $2}' | xargs sudo kill
-ps -ef | grep "python trainer.py" | awk '{print $2}' | xargs sudo kill
-ps -ef | grep "python checker.py" | awk '{print $2}' | xargs sudo kill
-

7b4b2860f5f85905f9f61f53e601d8f0b4c5e1de
FRTNX <frtnxwolfone@outlook.com>
2017-12-04 12:00:22 +0200

    Add files via upload

--- grassroot-nlu/activate_me.sh
+++ grassroot-nlu/activate_me.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
+
+# Docker aws configurtion
+#export AWS_ACCESS_KEY_ID=INCASEYOUNEEDAPROTIP
+#export AWS_SECRET_ACCESS_KEY=THESEARENOTREALKEYS
+#export AWS_DEFAULT_REGION=eu-west-1
+#export AWS_DEFAULT_OUTPUT=json 
+#aws ecr get-login --region eu-west-1
+
+
 export FLASK_APP=start_application.py
 flask run --host=0.0.0.0 > output.txt 2>&1 &
 python trainer.py > training_output.txt 2>&1 &
 python checker.py > system_status.txt 2>&1 &
+tail -f output.txt
--- grassroot-nlu/install_dependencies.sh
+++ grassroot-nlu/install_dependencies.sh
@@ -1,27 +1,59 @@
 #!/bin/sh
 
-pip install flask
+sudo apt-get update
 
-pip install pymongo
+if hash pip; then 
+    echo "pip is installed"
+else
+    sudo apt install python3-pip
+fi
 
-pip install schedule
+if hash git; then 
+    echo "git is installed"
+else
+    sudo apt install git
+fi
 
-pip install boto3
+# conda install libgcc
 
-pip install rasa_nlu
+# jvm
 
-pip install duckling
+pip3 install flask
 
-pip install git+https://github.com/mit-nlp/MITIE.git
 
-pip install psutil
+pip3 install pymongo
 
-wget -P ./ https://github.com/mit-nlp/MITIE/releases/download/v0.4/MITIE-models-v0.2.tar.bz2
+
+pip3 install schedule
+
+
+pip3 install boto3
+
+
+pip3 install rasa_nlu
+
+
+pip3 install duckling
+
+
+pip3 install git+https://github.com/mit-nlp/MITIE.git
+
+
+pip3 install psutil
+
+
+[ -f MITIE-models-v0.2.tar.bz2 ] && echo "Requirement already satisfied
+ MITIE-models-v0.2.tar.bz2" || wget -P ./ https://github.com/mit-nlp/MITIE/releases/download/v0.4/MITIE-models-v0.2.tar.bz2
+ 
 
 tar xvjf MITIE-models-v0.2.tar.bz2 --directory ./model/
 
-# insert here generating metadata, etc
 
-mkdir nsa
-
-touch nsa/event_listener.txt
+if hash aws; then
+    echo "aws is installed.
+    You're ready to go. Run 'sudo bash activate_me.sh' to fire up the API."
+else
+	sudo apt install awscli;
+	echo "Your AWS credentials are not configured.
+	Please run 'aws configure' and enter the associated details."
+fi

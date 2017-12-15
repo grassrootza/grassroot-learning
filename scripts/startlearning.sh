1f354522a7bd554f5b151f583e859eb7249672ae
FRTNX <frtnxwolfone@outlook.com>
2017-12-15 17:07:13 +0200

    Delete startlearning.sh

--- scripts/startlearning.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-. /etc/environment
-
-CURR=$PWD
-cd /var/grassroot-learning
-nohup java -jar build/libs/gs-rest-service-0.1.0.jar > grassroot-learning.log 2>&1 &
-echo $! > .pid
-cd $CURR

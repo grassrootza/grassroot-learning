a788958c056de8b7d2f73e65557593727b6c3aba
FRTNX <frtnxwolfone@outlook.com>
2018-06-20 16:48:06 +0200

    CI2 test 5

--- buildDocker.sh
+++ buildDocker.sh
@@ -2,7 +2,6 @@
 
 sudo docker build -t terafirma .
 
-sudo docker tag terafirma grassrootdocker/terafirma:latest
+sudo docker tag terafirma grassrootdocker/terafirma:skywalker
 
-sudo docker push grassrootdocker/terafirma:latest
-
+sudo docker push grassrootdocker/terafirma:skywalker

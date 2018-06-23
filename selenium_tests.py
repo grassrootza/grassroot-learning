dbd38f7bedc5dbd01036f96d97d0af289817cd07
FRTNX <frtnxwolfone@outlook.com>
2018-06-23 16:09:31 +0200

    adding selenium tests

--- selenium_tests.py
+++ selenium_tests.py
@@ -9,12 +9,13 @@ def start_light_app():
 def test_light_build():
     driver = webdriver.Chrome(service_args=['--verbose'])
     threading.Thread(target=start_light_app).start()
-    time.sleep(3)
+    time.sleep(15)
     driver.get('http://localhost:5000/')
+    driver.get('http://localhost:5000/datetime?date_string=tomorrow')
     print('driver: %s' % driver)
     print('\n\ndriver.title: %s' % driver.title)
     #assert "Grassroot-nlu" in driver.title
     time.sleep(5)
-    os.system('curl 127.0.0.1:5000/shutdown')
+    driver.get('http://127.0.0.1:5000/shutdown')
 
 test_light_build()

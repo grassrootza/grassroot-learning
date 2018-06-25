d223a17de7a53150ce37f9a9be3211e14988fc3b
FRTNX <frtnxwolfone@outlook.com>
2018-06-25 16:19:20 +0200

    preparring for nlu expansion

--- selenium_tests.py
+++ selenium_tests.py
@@ -11,16 +11,20 @@ def test_light_build():
     driver = webdriver.Chrome(service_args=['--verbose'])
     threading.Thread(target=start_light_app).start()
     time.sleep(40)
-    driver.get('http://localhost:5000/')
+    # driver.get('http://localhost:5000/')
     x = requests.get('http://localhost:5000/').content
     print('x: %s' % x)
-    if '404' in driver.title:
+    if 'What would you like to do' in str(x):
+        print('home page retrieved successfully')
+    else:
     	raise ValueError("path not available.")
 
-    driver.get('http://localhost:5000/datetime?date_string=tomorrow')
+    # driver.get('http://localhost:5000/datetime?date_string=tomorrow')
     y = requests.get('http://localhost:5000/datetime?date_string=tomorrow').content
     print('y: %s' % y)
-    if '404' in driver.title:
+    if len(y) == 16:
+        print('datetime engine seems operational')
+    else:
     	raise ValueError("path not available.")
 
     driver.get('http://127.0.0.1:5000/shutdown')

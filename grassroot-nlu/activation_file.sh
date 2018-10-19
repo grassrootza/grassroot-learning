7856757c31ffd449f96cd12bff120e8d0059ec4a
luke-grassroot <luke.jordan@grassroot.org.za>
2018-10-19 11:17:23 +0200

    Clear out of much that is now redundant, on way to mainstreaming this

--- grassroot-nlu/activation_file.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/sh
-
-export FLASK_APP=main.py
-flask run --host=0.0.0.0 --port=80
--- grassroot-nlu/logger.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-logFormatter = logging.Formatter("[NLULOGS] %(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s")
-rootLogger = logging.getLogger()
-rootLogger.setLevel(logging.DEBUG)
-
-fileHandler = logging.FileHandler("{0}/{1}.log".format('logs/', 'nlu'))
-fileHandler.setFormatter(logFormatter)
-rootLogger.addHandler(fileHandler)
